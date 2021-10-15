6436368f72415798b93015e6654648a9c458c935
chris-g2v <chris@g2voptics.com>
2021-10-15 16:12:09 -0600

    - Removed unneeded code from setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,6 @@ import setuptools
 
 version = "1.0.2"
 
-# if __name__ == "__main__":
-#     setuptools.setup(
-#         name="G2VPico",
-#         version=version,
-#         description="Python API library for interaction with G2V Optics Pico",
-#         author="Chris Woloschuk",
-#         author_email="chris@g2voptics.com",
-#         url="https://github.com/g2v-optics/G2VPico",
-#         python_requires=">3.6",
-#         packages=setuptools.find_packages(),
-#     )
-
-
 setuptools.setup(
     name="G2VPico",
     version=version,
