00af1b999587b84546748036636e6e49086461a6
chris-g2v <chris@g2voptics.com>
2021-10-15 15:36:13 -0600

    - Testing different setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,28 @@
 
 import setuptools
 
-version = "1.0.1"
+version = "1.0.2"
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name="G2VPico",
-        version=version,
-        description="Python API library for interaction with G2V Optics Pico",
-        author="Chris Woloschuk",
-        author_email="chris@g2voptics.com",
-        url="https://github.com/g2v-optics/G2VPico",
-        python_requires=">3.6",
-    )
+# if __name__ == "__main__":
+#     setuptools.setup(
+#         name="G2VPico",
+#         version=version,
+#         description="Python API library for interaction with G2V Optics Pico",
+#         author="Chris Woloschuk",
+#         author_email="chris@g2voptics.com",
+#         url="https://github.com/g2v-optics/G2VPico",
+#         python_requires=">3.6",
+#         packages=setuptools.find_packages(),
+#     )
+
+
+setuptools.setup(
+    name="G2VPico",
+    version=version,
+    description="Python API library for interaction with G2V Optics Pico",
+    author="Chris Woloschuk",
+    author_email="chris@g2voptics.com",
+    url="https://github.com/g2v-optics/G2VPico",
+    python_requires=">3.6",
+    packages=setuptools.find_packages(),
+)
